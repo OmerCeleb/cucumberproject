f014a6f8888c18c072ce6c1e11fee7bab9eb6138
OmerCeleb <omer534@outlook.com>
2024-03-23 15:39:08 +0100

    Cucumber_project_Day02

--- src/test/java/pages/DataTable_HomePage.java
+++ src/test/java/pages/DataTable_HomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,4 +9,35 @@ public class DataTable_HomePage {
     public DataTable_HomePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//span[text()=New']")
+    public WebElement newButton;
+
+    @FindBy(id = "DTE_Field_first_name")
+    public WebElement firstName;
+
+    @FindBy(id = "DTE_Field_last_name")
+    public WebElement lastName;
+
+    @FindBy(id = "DTE_Field_position")
+    public WebElement position;
+
+    @FindBy(id = "DTE_Field_office")
+    public WebElement office;
+
+    @FindBy(id = "DTE_Field_extension")
+    public WebElement extension;
+
+    @FindBy(id = "DTE_Field_start_date")
+    public WebElement startDate;
+
+    @FindBy(id = "DTE_Field_salary")
+    public WebElement salary;
+
+    @FindBy(xpath = "//button[@class='btn']")
+    public WebElement createButton;
+
+
+
+
 }
