77883ea48775a8f6267799dd680ce81879414116
OmerCeleb <omer534@outlook.com>
2024-03-26 14:22:12 +0100

    Cucumber_project_Day03

---
+++ src/test/java/stepdefinitions/BlueRentalStepDefs.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BlueRentalStepDefs {
+
+
+    @When("user enters admin email and password")
+    public void user_enters_admin_email_and_password(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+    @When("user clikcs on login button")
+    public void user_clikcs_on_login_button() {
+
+    }
+
+    @Then("verify login is successful")
+    public void verify_login_is_successful() {
+
+    }
+
+
+}
