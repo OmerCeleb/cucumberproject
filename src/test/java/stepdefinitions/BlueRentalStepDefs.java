497c8f31610c63faa26e87a849b11bc2aab7dfbb
OmerCeleb <omer534@outlook.com>
2024-03-26 14:32:19 +0100

    Cucumber_project_Day03

--- src/test/java/stepdefinitions/BlueRentalStepDefs.java
+++ src/test/java/stepdefinitions/BlueRentalStepDefs.java
@@ -2,23 +2,36 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.BlueRental_LoginPage;
+
+import java.util.List;
 
 public class BlueRentalStepDefs {
+
+    BlueRental_LoginPage blueRentalLoginPage = new BlueRental_LoginPage();
 
 
     @When("user enters admin email and password")
     public void user_enters_admin_email_and_password(io.cucumber.datatable.DataTable dataTable) {
+        List<String> credentials = dataTable.row(1);  // index starts at 0
+        System.out.println("credentials = " + credentials);  //  | jack1@gmail.com |  | 12345    |
+
+        blueRentalLoginPage.email.sendKeys(credentials.get(0));  // sends email dynamically
+        blueRentalLoginPage.password.sendKeys(credentials.get(1));  // sends password dynamically
+
 
     }
 
     @When("user clikcs on login button")
     public void user_clikcs_on_login_button() {
+        blueRentalLoginPage.loginButton.click();
 
     }
 
     @Then("verify login is successful")
     public void verify_login_is_successful() {
-
+        Assert.assertTrue(blueRentalLoginPage.adminProfile.isDisplayed());
     }
 
 
