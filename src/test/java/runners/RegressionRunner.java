e6df7a6d25908c586f9f60dd16d2aa858db61689
OmerCeleb <omer534@outlook.com>
2024-04-10 10:06:09 +0200

    Cucumber_project_Day05

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -18,9 +18,9 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",// provides path of the features folder
         glue = {"stepdefinitions", "hooks"}, // provides path of the step definitions folder and hooks folder
         dryRun = false, // dryRun = true generates the missing step definitions, without running the existing step definitions
-        tags = "@smoke"
+        tags = "@regression"
 
 
 )
-public class SmokeRunner {
+public class RegressionRunner {
 }
