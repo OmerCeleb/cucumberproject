c044ea7153a88c66132ea3d6d6c0aaa3b521059b
OmerCeleb <omer534@outlook.com>
2024-04-22 18:35:02 +0200

    Cucumber_project_Day08

--- src/test/java/stepdefinitions/DBStepDefs.java
+++ src/test/java/stepdefinitions/DBStepDefs.java
@@ -1,8 +1,12 @@
 package stepdefinitions;
 
+import com.mongodb.DB;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import utilities.DBUtils;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class DBStepDefs {
 
@@ -16,7 +20,8 @@ public class DBStepDefs {
     @Given("user gets the column {string} from the table {string}")
     public void user_gets_the_column_from_the_table(String columnName, String tableName) {
     //    DBUtils.executeQuery("Select * FROM jhi_user");  // HARD CODED
-        DBUtils.executeQuery("Select * FROM jhi_user");  // HARD CODED
+        DBUtils.executeQuery("Select" +columnName+" FROM "+tableName);
+
 
     }
 
@@ -27,4 +32,14 @@ public class DBStepDefs {
     }
 
 
+    @And("user read all column {string} data")
+    public void user_read_all_column_data(String columnName) throws SQLException {
+        DBUtils.getResultset().next(); // cheking if the data exists or not
+
+        // Get the data
+        Object obj1 = DBUtils.getResultset().getObject(columnName);
+        System.out.println("obj1 = " + obj1);
+
+
+    }
 }
--- src/test/resources/features/db_features/db.feature
+++ src/test/resources/features/db_features/db.feature
@@ -4,6 +4,7 @@ Feature: jdbc_feature
   Scenario: TC01_read_user_login
     Given user connects to application database
     And user gets the column "*" from the table "jhi_user"
+    And user read all column "login" data
 
 
     #   And close the database
