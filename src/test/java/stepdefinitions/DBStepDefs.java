4a8bd0dbd406948f01bfbfad5b7fe1e59e2f184c
OmerCeleb <omer534@outlook.com>
2024-04-22 19:08:00 +0200

    Cucumber_project_Day08

--- src/test/java/stepdefinitions/DBStepDefs.java
+++ src/test/java/stepdefinitions/DBStepDefs.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 import com.mongodb.DB;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import utilities.DBUtils;
 
 import java.sql.SQLException;
@@ -11,7 +12,7 @@ import java.util.List;
 public class DBStepDefs {
 
     @Given("user connects to application database")
-    public void user_connects_to_application_database(){
+    public void user_connects_to_application_database() {
         DBUtils.createConnection();
 
 
@@ -19,8 +20,8 @@ public class DBStepDefs {
 
     @Given("user gets the column {string} from the table {string}")
     public void user_gets_the_column_from_the_table(String columnName, String tableName) {
-    //    DBUtils.executeQuery("Select * FROM jhi_user");  // HARD CODED
-        DBUtils.executeQuery("Select" +columnName+" FROM "+tableName);
+        //    DBUtils.executeQuery("Select * FROM jhi_user");  // HARD CODED
+        DBUtils.executeQuery("Select" + columnName + " FROM " + tableName);
 
 
     }
@@ -39,6 +40,34 @@ public class DBStepDefs {
         // Get the data
         Object obj1 = DBUtils.getResultset().getObject(columnName);
         System.out.println("obj1 = " + obj1);
+        // -----------------------------------------------------------
+        DBUtils.getResultset().next(); // to move to the next data from the database, you must use next() method
+        Object obj2 = DBUtils.getResultset().getObject(columnName);
+        System.out.println(obj2);
+
+        // -----------------------------------------------------------
+        DBUtils.getResultset().next(); // to move to the next data from the database, you must use next() method
+        Object obj3 = DBUtils.getResultset().getObject(columnName);
+        System.out.println(obj3);
+        // -----------------------------------------------------------
+        // To go to a specific row
+        DBUtils.getResultset().absolute(4); //
+        Object obj4 = DBUtils.getResultset().getObject(columnName);
+        System.out.println(obj4);
+        // -----------------------------------------------------------
+        // Using while loop
+        int rowNum = 8;
+        while (DBUtils.getResultset().next()) {
+            Object currentRowData = DBUtils.getResultset().getObject(columnName);
+            System.out.println(currentRowData);
+            rowNum++;
+        }
+
+
+    }
+
+    @Then("verify table {string} and column {string} contains {string}")
+    public void verify_table_and_column_contains(String tableName, String columnName, String data) {
 
 
     }
--- src/test/resources/features/db_features/db.feature
+++ src/test/resources/features/db_features/db.feature
@@ -5,7 +5,7 @@ Feature: jdbc_feature
     Given user connects to application database
     And user gets the column "*" from the table "jhi_user"
     And user read all column "login" data
-
+    Then verify table "jhi_user" and column "login" contains "scott"
 
     #   And close the database
 
